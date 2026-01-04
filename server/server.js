96aaec030cfe9bdb5c4726c858566393e5bb00ca
Sakshi-Kumari07 <sakshi.k@testyantra.in>
2026-01-04 22:41:32 +0530

    fix cors for vercel

--- server/server.js
+++ server/server.js
@@ -4,29 +4,25 @@ const cors = require("cors");
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
 
-// ✅ CORS (MOST IMPORTANT)
 server.use(
   cors({
-    origin: "https://event-pro-8z18-git-main-sakshis-projects-8cf9076.vercel.app",
+    origin: true,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-// Default middlewares
-server.use(middlewares);
+server.options("*", cors());
 
-// Required for json-server-auth
+server.use(jsonServer.defaults());
+
 server.db = router.db;
 
-// Auth routes
 server.use(auth);
-
-// CRUD routes
 server.use(router);
 
-// ✅ Use Render PORT
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log("Auth Server running");
