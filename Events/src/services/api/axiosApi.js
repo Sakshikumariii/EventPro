5b27402b7b8164ed7d84949cdef8accb9a072281
Sakshi-Kumari07 <sakshi.k@testyantra.in>
2026-01-05 23:20:55 +0530

    feat: Added favicon Icon

--- Events/src/services/api/axiosApi.js
+++ Events/src/services/api/axiosApi.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-if (!API_URL) {
-  throw new Error("VITE_API_URL is not defined");
-}
 
 const axiosApi = axios.create({
   baseURL: API_URL,
